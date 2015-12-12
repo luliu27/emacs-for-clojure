b026df8accd74cd9eb372c311d6a55d2264ecc63
Lu Liu <luliu27@gmail.com>
2015-12-12 12:20:01 -0500

    use melpa-stable; add clj-refactor package

--- init.el
+++ init.el
@@ -8,9 +8,10 @@
              '("marmalade" . "http://marmalade-repo.org/packages/") t)
 (add-to-list 'package-archives
              '("tromey" . "http://tromey.com/elpa/") t)
+;; (add-to-list 'package-archives
+;;             '("melpa" . "http://melpa.milkbox.net/packages/") t)
 (add-to-list 'package-archives
-             '("melpa" . "http://melpa.milkbox.net/packages/") t)
-
+             '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 ;; (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
 ;;                          ("marmalade" . "http://marmalade-repo.org/packages/")
 ;;                          ("melpa" . "http://melpa-stable.milkbox.net/packages/")))
