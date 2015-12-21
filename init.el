5b24cbd868ccbf98b5a5bdf85efb4b87a8ca3897
Lu Liu <luliu27@gmail.com>
2015-12-21 06:29:37 -0500

    use different melpa endpoint

--- init.el
+++ init.el
@@ -8,8 +8,8 @@
              '("marmalade" . "http://marmalade-repo.org/packages/") t)
 (add-to-list 'package-archives
              '("tromey" . "http://tromey.com/elpa/") t)
-;; (add-to-list 'package-archives
-;;             '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages") t)
 (add-to-list 'package-archives
              '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 ;; (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
